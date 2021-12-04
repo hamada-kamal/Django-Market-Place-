64a24be70d2bfb297521b4caaa800d121cf01cf3
hamada-kamal <hamadakamal819gmail.com>
2021-12-04 17:20:21 +0530

    make =>> debug = True

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t9a700)j=1kw98p-v5bu+b(294pp#qrs%jx3@@_3+=zc@_$-&b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['elsouq.herokuapp.com','127.0.0.1']
 
